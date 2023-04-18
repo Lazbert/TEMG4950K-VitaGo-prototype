e46dd69017aa2c34b38c47d8675d48694a8100fe
Lazbert <85731679+Lazbert@users.noreply.github.com>
2023-04-18 21:11:39 +0800

    feat: haha removal goes brhhhh

--- frontend/src/components/PastSupplementItem.tsx
+++ frontend/src/components/PastSupplementItem.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { ReactComponent as IconSmallTick } from "@/assets/icons/IconSmallTick.svg";
+import cx from "classnames";
+
 interface PastSupplementsItemProps {
   Display?: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & {
@@ -8,6 +12,8 @@ interface PastSupplementsItemProps {
   name: string;
   brand: string;
   isFirst: boolean;
+  selected: Array<string>;
+  setSelected: React.Dispatch<React.SetStateAction<Array<string>>>;
 }
 
 export const PastSupplementsItem: React.FC<PastSupplementsItemProps> = ({
@@ -16,19 +22,43 @@ export const PastSupplementsItem: React.FC<PastSupplementsItemProps> = ({
   name,
   brand,
   isFirst,
+  selected,
+  setSelected,
 }) => {
+  const [checked, setChecked] = useState(false);
+
+  const onClickHandler = () => {
+    if (checked) {
+      const updated = selected.filter((item) => item != name);
+      setSelected(updated);
+    } else {
+      setSelected([...selected, name]);
+    }
+    setChecked(!checked);
+  };
+
   return (
     <div className="flex flex-col items-center">
       {!isFirst && <div className="w-[95%] border-t border-grey my-6" />}
       <div className="flex items-center gap-4">
         {Display && <Display className={displayClassName} />}
-        <div className="flex flex-col justify-center">
-          <span className="text-[18px] font-bold">
+        <div className="w-[236px] flex flex-col justify-center">
+          <span className="text-[16px] font-bold">
             {name} - {brand}
           </span>
-          <span className="font-bold text-[#989191]">2023 March</span>
+          <span className="font-medium text-[#989191]">2023 March</span>
         </div>
-        <div className="min-w-[30px] min-h-[30px] rounded-full border border-primaryBlue"></div>
+        <button
+          onClick={onClickHandler}
+          className={cx(
+            "min-w-[30px] min-h-[30px] rounded-full border border-primaryBlue",
+            { "bg-primaryBlue": checked }
+          )}
+        >
+          {checked && (
+            <IconSmallTick className="mx-auto my-auto w-[20px] h-[20px]" />
+          )}
+        </button>
       </div>
     </div>
   );
